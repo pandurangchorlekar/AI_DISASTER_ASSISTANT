69bafc19b6c98f08962a386fb44cbc5e1db5d1d0
pandurangchorlekar <pandurangchorlekar94@gmai.com>
2025-09-15 09:34:58 +0530

    your update message

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
-app = Flask(__name__)
+app = Flask(gunicorn app:app)
 CORS(app)
 
 # --- API Key for Weather ---
